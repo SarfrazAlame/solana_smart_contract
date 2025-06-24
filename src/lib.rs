eb5dc72a176cee483a70ff47d1f4c6d8e047b408
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-06-24 09:26:22 +0530

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+enum InstructionType {
+    Increment(u32),
+    Decrement(u32)
+}
+
+entrypoint!(counter_contract);
+
+pub fn counter_contract(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?;
+
+    let instruction_type = 
+
+}
